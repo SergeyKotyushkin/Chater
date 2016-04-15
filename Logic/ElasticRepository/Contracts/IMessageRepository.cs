bca80f605958069d2aacfe64a19be6f2fec0af36
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2016-04-15 17:49:42 +0300

    Sending messages to everyone

---
+++ Logic/ElasticRepository/Contracts/IMessageRepository.cs
@@ -0,0 +1,11 @@
+﻿using Logic.Models;
+
+namespace Logic.ElasticRepository.Contracts
+{
+    public interface IMessageRepository
+    {
+        ElasticResult Add(string chatGuid, string userGuid, string text);
+
+        ElasticResult GetByChatId(string guid);
+    }
+}
